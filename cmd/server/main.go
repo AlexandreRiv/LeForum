0530508381e60ea4a3bed7917ef3a340c99d0a9f
Enoxboo <matteo.marquant@ynov.com>
2025-06-03 13:58:58 +0200

    feature(post_page): add post page to localhost

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,16 +5,21 @@ import (
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func indexHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("web/templates/index.html"))
+	tmpl.Execute(w, nil)
+}
+
+func postHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("web/templates/post_page.html"))
 	tmpl.Execute(w, nil)
 }
 
 func main() {
 	fs := http.FileServer(http.Dir("web/static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/post", postHandler)
 
 	http.ListenAndServe(":3002", nil)
 }
-
