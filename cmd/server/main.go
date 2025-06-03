1b1a14e3b11ed72b250a7e5086a43d418a29f01c
Alexandre <alexandreriviere111@gmail.com>
2025-06-03 14:40:26 +0200

    feat(page)link all the page together

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -10,6 +10,8 @@ func main() {
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	http.HandleFunc("/", api.Handler)
 	http.HandleFunc("/toggle-theme", api.ToggleThemeHandler)
+	http.HandleFunc("/post", api.PostHandler)
+	http.HandleFunc("/categories", api.CategoriesHandler)
 
 	http.ListenAndServe(":3002", nil)
 }
