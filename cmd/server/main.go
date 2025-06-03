25d9e858298358dee53ba1d553a997bea92b6438
Enoxboo <matteo.marquant@ynov.com>
2025-06-03 13:58:58 +0200

    feature(post_page): add post page to localhost

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,15 +1,25 @@
 package main
 
 import (
-	"LeForum/internal/api"
+	"html/template"
 	"net/http"
 )
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("web/templates/index.html"))
+	tmpl.Execute(w, nil)
+}
+
+func postHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("web/templates/post_page.html"))
+	tmpl.Execute(w, nil)
+}
 
 func main() {
 	fs := http.FileServer(http.Dir("web/static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
-	http.HandleFunc("/", api.Handler)
-	http.HandleFunc("/toggle-theme", api.ToggleThemeHandler)
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/post", postHandler)
 
 	http.ListenAndServe(":3002", nil)
 }
