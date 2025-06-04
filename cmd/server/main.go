b852010b6e5621c39172994a84f5f7a105ef6489
zoom26042604 <nathan.ferre@ynov.com>
2025-06-04 21:01:33 +0200

    refactor: improve error messages and code readability in main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"LeForum/internal/api"
 	"LeForum/internal/auth"
 	"LeForum/internal/storage"
 	"html/template"
 	"log"
 	"net/http"
 	"time"
+
 	"github.com/joho/godotenv"
 )
 
@@ -18,43 +20,45 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("web/templates/index.html"))
 	err := tmpl.Execute(w, nil)
 	if err != nil {
-		log.Printf("Error executing template: %v", err)
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		log.Printf("Erreur lors de l'exécution du template: %v", err)
+		http.Error(w, "Erreur Interne du Serveur", http.StatusInternalServerError)
 	}
 }
 
 func init() {
-	err := godotenv.Load()
-	if err != nil {
+	if err := godotenv.Load(); err != nil {
 		log.Println("Pas de fichier .env chargé")
 	}
 }
 
 func main() {
+	// Initialisation de la base de données
 	if err := storage.InitDB(); err != nil {
-        	log.Fatalf("Erreur lors de l'initialisation de la base de données: %v", err)
-        }
+		log.Fatalf("Erreur lors de l'initialisation de la base de données: %v", err)
+	}
 
-	// Créer le multiplexeur
+	// Créer le ServeMux
 	mux := http.NewServeMux()
 
 	// Serveur de fichiers statiques
 	fs := http.FileServer(http.Dir("web/static"))
 	mux.Handle("/static/", http.StripPrefix("/static/", fs))
 
-	// Route de base
+	// Routes principales
 	mux.HandleFunc("/", indexHandler)
+	mux.HandleFunc("/toggle-theme", api.ToggleThemeHandler)
 
-	// Configuration de l'authentification
+	// Authentification
 	authHandler := auth.NewHandler()
 	authHandler.RegisterRoutes(mux)
 
+	// Nettoyage des sessions expirées périodiquement
 	go func() {
-          ticker := time.NewTicker(1 * time.Hour)
-          for range ticker.C {
-            auth.CleanExpiredSessions()
-          }
-    	}()
+		ticker := time.NewTicker(1 * time.Hour)
+		for range ticker.C {
+			auth.CleanExpiredSessions()
+		}
+	}()
 
 	// Démarrage du serveur
 	log.Println("Serveur démarré sur http://localhost:3002")
