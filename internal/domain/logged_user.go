fe99cd8817b7505705a09a38161c3e50c32af442
zoom26042604 <nathan.ferre@ynov.com>
2025-06-12 16:13:53 +0200

    refactor(logged_user): simplify role type import in LoggedUser struct

--- internal/domain/logged_user.go
+++ internal/domain/logged_user.go
@@ -1,13 +1,12 @@
 package domain
 
 import (
-	"LeForum/internal/api/middleware"
 	"time"
 )
 
 type LoggedUser struct {
-	Email     string              `json:"email"`
-	Name      string              `json:"name"`
-	LoginTime time.Time           `json:"login_time"`
-	Role      middleware.RoleType `json:"role"`
+	Email     string    `json:"email"`
+	Name      string    `json:"name"`
+	LoginTime time.Time `json:"login_time"`
+	Role      RoleType  `json:"role"`
 }
